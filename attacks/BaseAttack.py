c5f705121501cd92eaf4438cf08a1a59b318f1e9
Alasdair Campbell <alicampbellkhaya@gmail.com>
2023-11-13 13:40:05 -0500

    added FGSM

---
+++ attacks/BaseAttack.py
@@ -0,0 +1,54 @@
+import torch
+
+class BaseAttack():
+    def __init__(self, name, model, device):
+        self.attack = name
+        self.model = model
+        self.device = device
+
+    def forward(self, images, labels=None):
+        """Should be overwritten by every subclass"""
+        raise NotImplementedError
+    
+    @torch.no_grad()
+    def get_output_with_eval_nograd(self, inputs):
+        given_training = self.model.training
+        if given_training:
+            self.model.eval()
+        outputs = self.get_logits(inputs)
+        if given_training:
+            self.model.train()
+        return outputs
+    
+    @torch.no_grad()
+    def get_least_likely_label(self, inputs, labels=None):
+        outputs = self.get_output_with_eval_nograd(inputs)
+        if labels is None:
+            _, labels = torch.max(outputs, dim=1)
+        n_classses = outputs.shape[-1]
+
+        target_labels = torch.zeros_like(labels)
+        for counter in range(labels.shape[0]):
+            l = list(range(n_classses))
+            l.remove(labels[counter])
+            _, t = torch.kthvalue(outputs[counter][l], self._kth_min)
+            target_labels[counter] = l[t]
+
+        return target_labels.long().to(self.device)
+
+    @torch.no_grad()
+    def get_random_label(self, inputs, labels=None):
+        outputs = self.get_output_with_eval_nograd(inputs)
+        if labels is None:
+            _, labels = torch.max(outputs, dim=1)
+        n_classses = outputs.shape[-1]
+
+        target_labels = torch.zeros_like(labels)
+        for counter in range(labels.shape[0]):
+            l = list(range(n_classses))
+            l.remove(labels[counter])
+            t = (len(l) * torch.rand([1])).long().to(self.device)
+            target_labels[counter] = l[t]
+
+        return target_labels.long().to(self.device)
+
--- attacks/fgsm.py
+++ attacks/fgsm.py
@@ -1,18 +1,29 @@
 import torch
+import BaseAttack
 
-class FGSM():
-    def __init__(self, model, epsilons, device):
-        self.model = model
-        self.epsilons = epsilons
-        self.device = device
+class FGSM(BaseAttack):
+    def __init__(self, model, device, eps):
+        super().__init__("FGSM", model, device)
+        self.eps = eps
 
-    def forward(self, images, labels):
-        pass
+    def forward(self, image, label):
+        image_grad = image.grad.data
+        image_denorm = self.denorm(image)
+
+        sign_data_grad = image_grad.sign()
+
+        perturbed_image = image + self.eps * sign_data_grad
+        perturbed_image = torch.clamp(perturbed_image, 0, 1)
+
+        return perturbed_image
+
 
     # Restores tensors to original levels
     def denorm(self, batch, mean=[0.1307], std=[0.3081]):
         if isinstance(mean, list):
-            mean = torch.tensor(mean).to(device)
+            mean = torch.tensor(mean).to(self.device)
         if isinstance(std, list):
-            std = torch.tensor(std).to(device)
+            std = torch.tensor(std).to(self.device)
+
+        return batch * std.view(1, -1, 1, 1) + mean.view(1, -1, 1, 1)
     
